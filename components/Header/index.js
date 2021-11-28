947c36dea82cbd62f4dc35929754066ba3a134be
imsdolta <sachin.dolta@vedana.studio>
2021-11-28 22:17:47 +0530

    Update title and add icon

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Box, Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/PanoramaPhotosphere';
 
 const Header = (props) => {
   const router = useRouter();
-  const { sections, title } = props;
+  const { title } = props;
 
   return (
     <Box
       sx={{
         height: 80,
         width: 'auto',
+        display: 'flex',
+        gap: '10px',
+        alignItems: 'center',
         backgroundColor: 'primary',
+        paddingLeft: '55px',
       }}
     >
+      <HomeIcon fontSize='large' />
       <Typography
         onClick={() => {
           router.push('/');
         }}
         sx={{
+          paddingRight: '10px',
           cursor: 'pointer',
           '&:hover': {
             color: '#424961de',
           },
         }}
-        variant='h2'
-        component='h2'
+        variant='h4'
       >
         {title}
       </Typography>
